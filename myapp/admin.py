43dbf4f04346bb1effce18837211d46645552c99
Vanessa Campoe <nessa.campoe@gmail.com>
2026-04-20 14:30:59 -0400

    Completed 2.1 Models, Admin, and Seed Script

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import AttackCategory, SecurityEvent, DataIngestionLog
+
+#Requirement: customize 1 model in Django Admin
+@admin.register(SecurityEvent)
+class SecurityEventAdmin(admin.ModelAdmin):
+    list_display = ('category', 'timestamp', 'severity', 'threat_score', 'source')
+    list_filter = ('severity','source','category')
+    search_fields = ('category__name',)
+
+admin.site.register(AttackCategory)
+admin.site.register(DataIngestionLog)
+
+
+
 # Register your models here.
---
+++ myapp/management/commands/seed_data.py
@@ -0,0 +1,39 @@
+import csv
+import os
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from myapp.models import SecurityEvent, AttackCategory
+
+class Command(BaseCommand):
+    help = 'Seeds the database with Cybersecurity Attack dat from Project 1'
+
+    def handle(self, *args, **options):
+        # define path to CSV'
+        csv_file_path = os.path.join(settings.BASE_DIR,'..','data','raw','cybersecurity_attacks.csv')
+
+        self.stdout.write(f"Loading data from: {csv_file_path}")
+
+        try:
+            with open(csv_file_path, 'r') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    #handle ForeignKey parent - ensure category exsists before linking an event
+                    category_obj, _ = AttackCategory.objects.get_or_create(
+                        name=row['Attack Type'] #needs to match csv column name 
+                    )
+
+                    #requirement: use get_or_create to avoid duplicates
+                    SecurityEvent.objects.get_or_create(
+                        category=category_obj,
+                        timestamp = row['Timestamp'], #change to match csv
+                        defaults = {
+                            'severity' : row['Severity Level'].lower(),
+                            'threat_score':float(row['Anomaly Scores']),
+                            'source':'csv'
+                        }
+                    )
+            self.stdout.write(self.style.SUCCESS('Successfully seeded cybersecurity data.'))
+        except FileNotFoundError:
+            self.stderr.write(f"File notfound at {csv_file_path}")
+        except Exception as e:
+            self.stderr.write(f"An error occured: {e}")
